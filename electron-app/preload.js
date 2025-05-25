93d9f7ba9346712431f9a04e7f31531aa4afd2a2
anjaneyasoftware <anjaneyasoftware@gmail.com>
2025-05-25 15:33:18 +0530

    Delete electron-app directory

--- electron-app/preload.js
+++
@@ -1,7 +0,0 @@
-// preload.js (Make sure this file exists and is referenced in your BrowserWindow config)
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose `getSources` to the renderer process
-contextBridge.exposeInMainWorld('electron', {
-  getSources: (options) => ipcRenderer.invoke('getSources', options)
-});
